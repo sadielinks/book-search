b9e118e13cf7ae6c950a4e2d2bb21f0cdb4c03b8
Sadie Sial <sadiesial@gmail.com>
2022-01-07 01:27:05 -0600

    updated server.js to have apollo + auth + and PORT messages

--- server/server.js
+++ server/server.js
@@ -12,9 +12,17 @@ const { typeDefs, resolvers } = require("./schemas")
 // introduce authentication
 const { authMiddleware } = require('./utils/auth')
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// middleware + server connect
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,6 +34,14 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// build from the OG index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Now running API server on port ${PORT}!!!!`);
+    console.log(`Now use GraphQL at http://localhost:${PORT}${server.graphqlPath}!!!!`);
+  });
 });
