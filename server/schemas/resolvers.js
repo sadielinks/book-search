7d11599830deadcc18df0a2f58dd714e29a3d395
Sadie Sial <sadiesial@gmail.com>
2022-01-07 01:56:52 -0600

    class material pasted in for query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,3 +5,18 @@ const { User } = require('../models');
 // require auth middleware
 const { signToken } = require('../utils/auth');
 
+// 
+const resolvers = {
+    Query: {
+        // 'me' = user
+        me: async (parent, args, context) => {
+            if (context.user) {
+                // ripped from TA :)
+                const userData = await User.findOne({ _id: context.user._id }).select("-__v -password");
+                return userData;
+            }
+            throw new AuthenticationError('Please log into the application :)');
+        }
+    }
+
+}
