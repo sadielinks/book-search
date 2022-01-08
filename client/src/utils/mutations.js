a8db06a4e12626055c0bde119aa218d295d6dcf8
Sadie Sial <sadiesial@gmail.com>
2022-01-08 01:31:47 -0600

    LOGIN_USER for loginUser mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,17 @@
 import { gql } from "apollo-server-express";
 
 // loginUser
-
+export const LOGIN_USER = gql`
+mutation loginUser ($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`
 
 
 // addUser
