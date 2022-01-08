88fca4a597190d73546804400fea8bfb96cacaa8
Sadie Sial <sadiesial@gmail.com>
2022-01-08 01:26:33 -0600

    imported apollo + GET_ME query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,22 @@
 import { gql } from "apollo-server-express";
+
+// GET_ME will execute the "me" query set up with apollo server
+export const GET_ME = gql`
+{
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
