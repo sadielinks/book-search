8e5b25b35c651e6cf71c22a1dc3fe9747c5692aa
Sadie Sial <sadiesial@gmail.com>
2022-01-08 22:10:14 -0600

    reworked setContext + localStorage for token in header

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,24 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 // import apollo server deets
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient } from 'apollo-boost';
 
-// import setContext - included in authLink
-import { setContext } from '@apollo/client'
+const client = new ApolloClient({
+  request: (operation) => {
+    // use getItem method from local storage
+    const token = localStorage.getItem('id_token');
 
-const httpLink = createHttpLink({
+    // setContext obtains token and attaches to http headers
+    operation.setContext({
+      headers: {
+        // with token now obtained
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    });
+  },
   // url/endpoint for all GraphQL requests
   uri: '/graphql',
-});
-
-// setContext obtains token and attaches to http headers
-const authLink = setContext((_, { headers }) => {
-  // use getItem method from local storage
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      // with token now obtained
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// bring together the httpLink + authLink objects for API requests
-const client = new ApolloClient({
-  // combo
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
 });
 
 function App() {
