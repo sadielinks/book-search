b27942ba39446ee2addffdfff0a23d8dec18b2b1
Sadie Sial <sadiesial@gmail.com>
2022-01-09 00:35:34 -0600

    removed unused code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 // removed getMe + deleteBook } from utils/API
@@ -21,7 +21,7 @@ const SavedBooks = () => {
   const userData = data?.me || {};
 
   // useMutation w/ REMOVE_BOOK
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
